ea6e815ee19b1d1abfa803e7f531d11f23836dee
Nikhil-L <nikhil89515@gmail.com>
2020-04-03 20:51:53 +0530

    Add code to print function

--- Graphs/UnionFind.java
+++ Graphs/UnionFind.java
@@ -34,7 +34,11 @@ class UnionFind
 
 	public static void print(int a[], int n)
 	{
-
+		for(int i = 0; i < n; i++)
+		{
+			System.out.print(a[i] + " ");
+		}
+		System.out.println();
 	}
 
 	public static void main(String[]args)
