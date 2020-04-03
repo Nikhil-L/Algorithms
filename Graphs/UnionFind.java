92def77f7a1bbcb5ccf5f697855f335e60a043bd
Nikhil-L <nikhil89515@gmail.com>
2020-04-03 20:23:48 +0530

    Add initial find of Union Find algorithm

---
+++ Graphs/UnionFind.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.lang.*;
+
+class UnionFind
+{
+	public static void main(String[]args)
+	{
+		Scanner ip = new Scanner(System.in);
+		int n;
+		System.out.println("Enter the number of nodes : ");
+		n = ip.nextInt();
+		System.out.println(n);
+
+	}
+}
